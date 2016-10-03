6d4c2ddf6c4b7ade277a748567a7dba4ae5bec10
VINICIUS FERNANDO DA SILVA <11772052620@est.udicentro.local>
2016-10-03 11:09:56 -0300

    Criando uma excecao persnalizada de tabuleiro

---
+++ xadrez-aula-01/Tabuleiro/ExcecaoTabuleiro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TABULEIRO
+{
+    class ExcecaoTabuleiro : Exception
+    {
+        public excecaoTabuleiro(string msg) : base(msg)
+        {}
+    }
+}
--- xadrez-aula-01/Tabuleiro/Tabuleiro.cs
+++ xadrez-aula-01/Tabuleiro/Tabuleiro.cs
@@ -19,9 +19,21 @@ namespace TABULEIRO
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
         }
+
         public Peca peca(int linha, int coluna)
         {
             return pecas[linha, coluna];
+        }
+
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
         }
 
         public void colocarPeca(Peca P, Posicao pos)
@@ -30,5 +42,21 @@ namespace TABULEIRO
             P.posicao = pos;
         }
         
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.linha<0 || pos.linha>=linhas || pos.coluna<0 || pos.coluna>=colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if(!posicaoValida(pos))
+            {
+                throw new ExcecaoTabuleiro("Posição Inválida!!!");
+            }
+        }
     }
 }
